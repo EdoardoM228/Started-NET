245ad5ea5332da3f7a7d71ac0059fabb889a3152
EdoardoM <mihai_eduard@mail.ru>
2021-02-24 18:10:32 +0200

    Lesson-01-C#

--- Lessons-C/Program.cs
+++ Lessons-C/Program.cs
@@ -6,8 +6,27 @@ namespace Lessons_C
     { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Bye World!");
+            Console.Beep(786, 1000); // воспроизведение звука Console.Beep(int частота, int длительность)
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen; // Задаю параметр цвета букв (Темно-зеленный)
+            Console.BackgroundColor = ConsoleColor.Yellow; //Задаю параметр цвета заднего фона (Желтый) 
+
+            Console.WriteLine("Ya temno zelenii");
+            Console.WriteLine("I ya toje YrAAAA");
+
+            Console.ResetColor(); //Отменил прошлые параметры цвета 
+
+            Console.WriteLine("Blin a ya belii");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+            Console.Write("A net pokazalosi , ya ne belii");
+
             Console.ReadKey();
+
+
+
         }
     }
 }
