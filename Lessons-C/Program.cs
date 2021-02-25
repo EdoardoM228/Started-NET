c7f358526eed2c8078733f818a48cc5b0af22554
EdoardoM <mihai_eduard@mail.ru>
2021-02-25 21:29:57 +0200

    Lesson-02- C#

--- Lessons-C/Program.cs
+++ Lessons-C/Program.cs
@@ -6,22 +6,38 @@ namespace Lessons_C
     { 
         static void Main(string[] args)
         {
-            Console.Beep(786, 1000); // воспроизведение звука Console.Beep(int частота, int длительность)
+            byte a = 255; //struc Int32
+            sbyte b = -128; //struc Int32
+            ushort c = 65535; //struc Int32
+            short d = -32768; //struc Int32
 
-            Console.ForegroundColor = ConsoleColor.DarkGreen; // Задаю параметр цвета букв (Темно-зеленный)
-            Console.BackgroundColor = ConsoleColor.Yellow; //Задаю параметр цвета заднего фона (Желтый) 
+            uint e = 4294967295; //struc UInt32
+            int f = -2147483648; //struc UInt32
 
-            Console.WriteLine("Ya temno zelenii");
-            Console.WriteLine("I ya toje YrAAAA");
+            ulong g = 18446744073709551615; //struc UInt64
+            long h = -9223372036854775808; //struc UInt64
 
-            Console.ResetColor(); //Отменил прошлые параметры цвета 
+            float i = (float)-1.12345; //произвел явный кастинг(опасный) из float в double
+            double j = -1.12345; //struc double
+            decimal k = -1.12345M; // поставил суффикс M для создание литерала decimal
 
-            Console.WriteLine("Blin a ya belii");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
 
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(e);
+            Console.WriteLine(f);
 
-            Console.Write("A net pokazalosi , ya ne belii");
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+
+
+
 
             Console.ReadKey();
 
