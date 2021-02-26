526180d491a1078d2b1d09459cceef1bbed2385a
EdoardoM <mihai_eduard@mail.ru>
2021-02-26 20:31:13 +0200

    Lesson-03-C#

--- Lessons-C/Program.cs
+++ Lessons-C/Program.cs
@@ -6,40 +6,28 @@ namespace Lessons_C
     { 
         static void Main(string[] args)
         {
-            byte a = 255; //struc Int32
-            sbyte b = -128; //struc Int32
-            ushort c = 65535; //struc Int32
-            short d = -32768; //struc Int32
+             float T_mo, T_tu, T_we, T_th, T_fr, T_sa, T_su, T_sum, T_avg;
+             int sevendays = 7;
 
-            uint e = 4294967295; //struc UInt32
-            int f = -2147483648; //struc UInt32
-
-            ulong g = 18446744073709551615; //struc UInt64
-            long h = -9223372036854775808; //struc UInt64
-
-            float i = (float)-1.12345; //произвел явный кастинг(опасный) из float в double
-            double j = -1.12345; //struc double
-            decimal k = -1.12345M; // поставил суффикс M для создание литерала decimal
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-
-            Console.WriteLine(e);
-            Console.WriteLine(f);
-
-            Console.WriteLine(g);
-            Console.WriteLine(h);
-
-            Console.WriteLine(i);
-            Console.WriteLine(j);
-            Console.WriteLine(k);
+             T_mo = Convert.ToSingle(Console.ReadLine());
+             T_tu = Convert.ToSingle(Console.ReadLine());
+             T_we = Convert.ToSingle(Console.ReadLine());
+             T_th = Convert.ToSingle(Console.ReadLine());
+             T_fr = Convert.ToSingle(Console.ReadLine());
+             T_sa = Convert.ToSingle(Console.ReadLine());
+             T_su = Convert.ToSingle(Console.ReadLine());
+             
+             T_sum = T_mo + T_tu + T_we + T_th + T_fr + T_sa + T_su;
+             T_avg = T_sum / sevendays;
 
 
-
+            Console.WriteLine(T_sum);
+            Console.WriteLine(T_avg);
 
             Console.ReadKey();
+
+
+
 
 
 
