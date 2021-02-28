1383555e30927be5d78735280f883d3d75cde80d
EdoardoM <mihai_eduard@mail.ru>
2021-02-28 21:47:10 +0200

    Interpolation

--- Lessons-C/Program.cs
+++ Lessons-C/Program.cs
@@ -6,37 +6,7 @@ namespace Lessons_C
     { 
         static void Main(string[] args)
         {
-            Console.WriteLine(Math.Pow(3, 4)); // возведение числа в степень
-
-            Console.WriteLine(Math.Sqrt(36)); // Нахождение числа из корня 
-
-            Console.WriteLine(Math.Log(36, 6)); // Нахождение опказателся степени 
-
-            string firstword, secondword, text;
             int augend, addend;
-
-           firstword = Console.ReadLine();
-           secondword = Console.ReadLine(); 
-
-           text = firstword + ' ' + secondword;
-           text += '!';
-
-           Console.WriteLine(text);
-
-            /*Первый метод решения через сразу ввод чисел 
-
-          //  Console.WriteLine("Введите первое слагаемое : ");
-           //  augend = Convert.ToInt32(Console.ReadLine());
-
-          //  Console.WriteLine("Введите второе слагаеоме : ");
-          //   addend = Convert.ToInt32(Console.ReadLine());
-
-           // int sum = augend + addend;
-
-           // Console.WriteLine(augend + "+" + addend + "=" +sum);
-
-            */
-
             Console.WriteLine("Введите первое слагаемое : ");
             string firstaugend = Console.ReadLine();
 
@@ -48,12 +18,19 @@ namespace Lessons_C
 
             int sum = augend + addend;
 
-            string result = string.Format("Итог суммы : {0} + {1} = {2}", augend, addend, sum);
+            string result = $"итог суммы : {augend} + {addend} = {sum}";
             Console.WriteLine(result);
 
-            /* Второй способ через конвертацию из string в Int32 */
+            double interpolationExpression = 0.123456789_12345678;
+            const int aligment = 30;
 
+            Console.WriteLine($"8. R format : {interpolationExpression,-aligment:R} : End.");
 
+            Console.WriteLine($"8. R format : {interpolationExpression,aligment:R} : End.");
+
+            Console.WriteLine(@"C:\windows\notepad.exe");
+            Console.WriteLine("C:\\winsows\\notepad.exe");
+            
             Console.ReadKey();
 
 
