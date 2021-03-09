2b4cab7dc3bcf3dcd5789fafa817684238f79240
EdoardoM <mihai_eduard@mail.ru>
2021-03-09 21:08:10 +0200

    else if

--- Lessons-C/Program.cs
+++ Lessons-C/Program.cs
@@ -6,9 +6,15 @@ namespace Lessons_C
     { 
         static void Main(string[] args)
         {
-
+            string countryCode = "Азербайджан 994 ,Молдова 373, Армения 374, Россия 7, Беларусь 375, Таджикистан 992, Казахстан 7,"+
+            " Туркменистан 993 , Кыргызстан 996, Украина 380, Латвия 371, Узбекистан 998, Литва 370, Эстония 372";
             decimal plitkaKoliscestvo, plitkaCena;
+
+            Console.WriteLine(countryCode);
+           
             {
+                Console.WriteLine("Введите телефонный код страны : ");
+                countryCode = Console.ReadLine();
                 Console.WriteLine("Введите кол-во плитки : ");
                 plitkaKoliscestvo = Convert.ToUInt32(Console.ReadLine());
                 Console.WriteLine("Введите Цену плитки : ");
@@ -16,10 +22,95 @@ namespace Lessons_C
 
             }
 
-            decimal fullPriceTale;
+            decimal rate;
             {
-                fullPriceTale = plitkaCena * plitkaKoliscestvo;
+                const string AZEGBAINJAN_CODE = "994", ARMENIA_CODE = "374", BELARUS_CODE = "375", KAZAKHSTAN_CODE = "007", KYRGYZSTAN_CODE = "996",
+                             LATVIA_CODE = "371", LITHUANIA_CODE = "370", MOLDOVA_CODE = "373", RUSSIA_CODE = "007", TAJIKISTAN_CODE = "992",
+                             TURKMENISTAN_CODE = "993", UZBEKISTAN_CODE = "998", UKRAINE_CODE = "380", ESTONIA_CODE = "372";
+               
+                if (countryCode == AZEGBAINJAN_CODE)
+                {
+                    const decimal AZEGBAINJAN_RATE = 1.07m;
+                    rate = AZEGBAINJAN_RATE;
+                }
+                else if (countryCode == ARMENIA_CODE)
+                {
+                    const decimal ARMENIA_RATE = 0.95m;
+                    rate = ARMENIA_RATE;
+                }
+                else if (countryCode == BELARUS_CODE)
+                {
+                    const decimal BELARUS_RATE = 1;
+                    rate = BELARUS_RATE;
+                }
+                else if (countryCode == KAZAKHSTAN_CODE)
+                {
+                    const decimal KAZAKHSTAN_RATE = 0.79m;
+                    rate = KAZAKHSTAN_RATE;
+                }
+                else if (countryCode == KYRGYZSTAN_CODE)
+                {
+                    const decimal KYRGYSTAN_RATE = 0.83m;
+                    rate = KYRGYSTAN_RATE;
+                }
+                else if (countryCode == LATVIA_CODE)
+                {
+                    const decimal LATVIA_RATE = 1.12m;
+                    rate = LATVIA_RATE;
+                }
+                else if (countryCode == LITHUANIA_CODE)
+                {
+                    const decimal LITHUANIA_RATE = 1.12m;
+                    rate = LITHUANIA_RATE;
+                }
+                else if (countryCode == MOLDOVA_CODE)
+                {
+                    const decimal MOLDOVA_RATE = 0.97m;
+                    rate = MOLDOVA_RATE;
+                }
+                else if (countryCode == RUSSIA_CODE)
+                {
+                    const decimal RUSSIA_RATE = 1;
+                    rate = RUSSIA_RATE;
+                }
+                else if (countryCode == TAJIKISTAN_CODE)
+                {
+                    const decimal TAJIKISTAN_RATE = 0.76m;
+                    rate = TAJIKISTAN_RATE;
+                }
+                else if (countryCode == TURKMENISTAN_CODE)
+                {
+                    const decimal TURKMENISTAN_RATE = 0.81m;
+                    rate = TURKMENISTAN_RATE;
+                }    
+                else if (countryCode == UZBEKISTAN_CODE)
+                {
+                    const decimal UZBEKISTAN_RATE = 0.98m;
+                    rate = UZBEKISTAN_RATE;
+                }
+                else if (countryCode == UKRAINE_CODE)
+                {
+                    const decimal UKRAINE_RATE = 1m;
+                    rate = UKRAINE_RATE;
+                }
+                else if (countryCode == ESTONIA_CODE)
+                {
+                    const decimal ESTONIA_RATE = 1.12m;
+                    rate = ESTONIA_RATE;
+                }
+                else
+                {
+                   rate = 0;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Вы ввели не существующий код : {countryCode}!");
+                }
             }
+           
+            decimal cenaPlitkiWithRate = plitkaCena * rate;
+            decimal fullPriceTale = cenaPlitkiWithRate * plitkaKoliscestvo;
+            
+                
+            
 
             decimal priceDiscount;
             {
@@ -36,17 +127,15 @@ namespace Lessons_C
                         {
                             discountpersentage = 20;
                         }
+                        else if (discountFor50Pct)
+                        {
+                            discountpersentage = 50;
+                        }
                         else
                         {
-                            if (discountFor50Pct)
-                            {
-                                discountpersentage = 50;
-                            }
-                            else
-                            {
-                                discountpersentage = 0;
-                            }
+                            discountpersentage = 0;
                         }
+                   
                     }
                     priceDiscount = fullPriceTale / 100 * discountpersentage;
                 }
@@ -57,9 +146,10 @@ namespace Lessons_C
                 totalPriceTale = fullPriceTale - priceDiscount;
             }
 
-            Console.WriteLine($"Общая стоимость : {fullPriceTale}");
-            Console.WriteLine($"Сумма скидки : {priceDiscount}");
-            Console.WriteLine($"Сумма к оплате : {totalPriceTale}");
+            Console.WriteLine($"Цена с коэффициентом : {cenaPlitkiWithRate}  руб.");
+            Console.WriteLine($"Общая стоимость : {fullPriceTale}  руб.");
+            Console.WriteLine($"Сумма скидки : {priceDiscount} руб.");
+            Console.WriteLine($"Сумма к оплате : {totalPriceTale} руб.");
             
             Console.ReadKey();
 
