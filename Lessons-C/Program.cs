012233db7f4cd1cf75b16e709d305a8856a85a93
EdoardoM <mihai_eduard@mail.ru>
2021-03-14 22:36:19 +0200

    two-dimensional array

--- Lessons-C/Program.cs
+++ Lessons-C/Program.cs
@@ -6,56 +6,59 @@ namespace Lessons_C
     {
         static void Main(string[] args)
         {
-
+            int tipsPies, tipsIngredientsForPies;
             {
-                Console.Write("Введите кол-во элементов в массиве : ");
-                int sizeArray = Convert.ToInt32(Console.ReadLine());
-                int[] array = new int[sizeArray];
-                int index = 0;
-
-                while (index < array.Length)
-                {
-                    array[index] = index;
-                    Console.Write($"{array[index]}, ");
-                    index++;
-                }
-
-                Console.Write("\nВведите значение нового элемента массива : ");
-                int newElementArray = Convert.ToInt32(Console.ReadLine());
-
-                Array.Resize(ref array, array.Length + 1);
-                array[array.Length - 1] = newElementArray;
-
-                index = 0;
-
-                while (index < array.Length)
-                {
-                    Console.Write($"{array[index]}, ");
-                    index++;
-                }                 
-
-
-                Console.Write("\nВведи размер массива: ");
-                int n = Convert.ToInt32(Console.ReadLine());
-                int[] array2 = new int[100];
-                index = 0;
-
-                Console.Write("\nВведи значение в массив: ");
-                while (index < n)
-                {
-                    array2[index] = Convert.ToInt32(Console.ReadLine());
-                    index++;
-                }
-
-                Console.WriteLine($"Сумма 0-его значения и 1 значения массива { array2[0] + array2[1]}");
-
-
-                Console.ReadKey();
-
-
-                 
+                Console.Write("Введи кол-во видов пирожков : ");
+                tipsPies = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите кол-во требуемых ингредиентов : ");
+                tipsIngredientsForPies = Convert.ToInt32(Console.ReadLine());
             }
-            
+
+            decimal[,] ingredientsQuantity = new decimal[tipsPies, tipsIngredientsForPies];
+            decimal[,] costIngredients = new decimal[tipsIngredientsForPies, 1];
+            decimal[] Pies = new decimal[tipsPies];
+            decimal[,] costPie = new decimal[tipsPies, 1];
+
+            for (int i = 0; i < ingredientsQuantity.GetLength(0); i++)
+            {
+                for (int j = 0; j < ingredientsQuantity.GetLength(1); j++)
+                {
+                    Console.Write($"Введите кол-во ингрендиентов №{j + 1} для пирожков №{i + 1}(кг) : ");
+                    ingredientsQuantity[i, j] = Convert.ToDecimal(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < costIngredients.GetLength(0); i++)
+            {
+                Console.Write($"Введи цену ингредиента №{i + 1} : ");
+                costIngredients[i, 0] = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            for (int i = 0; i < Pies.Length; i++)
+            {
+                Console.Write($"Введи кол-во пирожков №{i + 1} : ");
+                Pies[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < ingredientsQuantity.GetLength(0); i++)
+            {
+                for (int j = 0; j < ingredientsQuantity.GetLength(1); j++)
+                {
+                    costPie[i, 0] += ingredientsQuantity[i, j] * costIngredients[j, 0];
+                }
+            }
+
+            decimal allCostPies = 0;
+
+            for (int i = 0; i < costPie.GetLength(0); i++)
+            {
+                allCostPies += Pies[i] * costPie[i, 0];
+            }
+
+            Console.WriteLine($"Общая стоимость пирожков : {allCostPies}");
+            Console.ReadKey(); 
         }
+            
     }
 }
