0a7c946757f567519e74284a153e3869edd900e2
EdoardoM <mihai_eduard@mail.ru>
2021-03-19 22:11:56 +0200

    Method-numberFibonacci

--- Lessons-C/Program.cs
+++ Lessons-C/Program.cs
@@ -4,7 +4,33 @@ namespace Lessons_C
 {
     class Program
     {
-      
+        static int NumbersFibonacciCycle(int number)
+        {
+            int n1 = 0, n2 = 1, n3 = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+            }
+            return n3;
+        }
+
+        static int NumbersFibonacci(int number)
+        {
+            if (number < 2)
+            {
+                return number;
+            }
+            else
+            {
+                return NumbersFibonacci(number - 1) + NumbersFibonacci(number - 2);
+            }
+        }
+
+
+
         static int Factorial(int number)
         {
             int fact = 1;
@@ -15,20 +41,33 @@ namespace Lessons_C
             }
             return fact;
         }
-        
-        
+
+
         static void Main(string[] args)
         {
-            while (true)
+            string agree;
+            do
             {
                 Console.Write("Введи число ,которого хочешь узнать факториал : ");
                 int factor = Convert.ToInt32(Console.ReadLine());
                 Console.Write($"{factor}! = {Factorial(factor)}");
 
-                Console.WriteLine("\nНажми любую клавишу для повторения");
+                Console.WriteLine("\nХодите получить число фибоначчи ?");
+                agree = Console.ReadLine();
                 Console.ReadKey();
+            } while (agree == "No");
+
+            Console.Write("Какое число Фибоначчи хочешь узнать : ");
+            int fibonacci = Convert.ToInt32(Console.ReadLine());
+            int sumFibonacci;
+            for (int i = 0; i < fibonacci; i++)
+            {
+                sumFibonacci = NumbersFibonacciCycle(i);
+                Console.Write($"{sumFibonacci}, ");
             }
+
         }
+
 
     }
 }
